b61f7a2ea7178a6d64b811326fc79aa5ab122f27
- <sangyeel87@naver.com>
2020-06-21 08:38:44 +0000

    데이터베이스에 stock code및 회사정보 저장과 admin에 등록

--- mysite/GetCompanyData.py
+++ mysite/GetCompanyData.py
@@ -1,11 +1,21 @@
 import pandas
-import os
 import requests
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","mysite.settings")
+
+
+import django
+django.setup()
+
+from companyData.models import CompanyData
+
 '''
 https://kind.krx.co.kr/corpgeneral/corpList.do?method=loadInitPage 홈페이지로 접속
 Excel 버튼을 누를떄 chrom http trace를 이용해서 post 정보를을 캡쳐
 해당 URL을 아래 __URL에 저장.
 '''
+
 class GetCompanyData:
     __URL  = ('http://kind.krx.co.kr/corpgeneral/corpList.do?'+
     'beginIndex=&'+
@@ -21,12 +31,25 @@ class GetCompanyData:
     'repIsuSrtCd:&'+
     'searchCodeType:&'+
     'searchType=13')
-    __SAVEDIR    = 'CompanyDataDir'
     
     def __init__(self):
-        print(GetCompanyData.__URL)
+        
         res = requests.get(GetCompanyData.__URL)
-        print(res.text)
-
+        #pandas read_html return list of datafram even if there are only one tr/td, 
+        #res data is looks like a HTML.
+        companyDataList = pandas.read_html(res.text,converters={'종목코드': str})
+        self.stockInfoDf = companyDataList[0] #Df is datafram
+        
+        for i in self.stockInfoDf.index:
+            name = self.stockInfoDf.loc[i, '회사명']
+            code = self.stockInfoDf.loc[i, '종목코드']
+            buz = self.stockInfoDf.loc[i, '업종']
+            prod = self.stockInfoDf.loc[i, '주요제품']
+            lsd = self.stockInfoDf.loc[i, '상장일']
+            ceo = self.stockInfoDf.loc[i, '대표자명']
+            site = self.stockInfoDf.loc[i, '홈페이지']
+            local = self.stockInfoDf.loc[i, '지역']
+            CompanyData(companyName = name).save()
+                        
 if __name__ == '__main__':
     companyData = GetCompanyData()
--- mysite/companyData/admin.py
+++ mysite/companyData/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import CompanyData
 # Register your models here.
+admin.site.register(CompanyData)
